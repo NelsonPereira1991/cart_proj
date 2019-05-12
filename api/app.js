fcc3000eec2655741235f1415da1830332e44a3f
Nelson Pereira <nelsonpereira1991@gmail.com>
2019-05-12 19:29:01 +0100

    Removing unnecessary files and replacing var declarations with let(from autogenerated files)

--- api/app.js
+++ api/app.js
@@ -1,16 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testAPI");
 let apiRouter = require("./routes/api");
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,10 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
 app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
