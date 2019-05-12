a2b4834f8d54a393457046c90a2c460f1b323043
Nelson Pereira <nelsonpereira1991@gmail.com>
2019-05-12 13:37:34 +0100

    Further work done

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
             bag: [],
             sumPrices: 0,
             products: [],
-            currentpage: "debugPage",
+            currentPage: 1,
             wishList: [],
             brands: []
         }
@@ -41,7 +41,18 @@ class App extends Component {
             .then(res => res.json())
             .then(jsonResponse =>
                 this.setState({
-                    brands: jsonResponse.data.wishList
+                    brands: jsonResponse.data.brands
+                })
+            )
+    }
+
+    callGetProductsRequest() {
+        fetch("http://localhost:9000/api/products")
+            .then(res => res.json())
+            .then(jsonResponse =>
+                this.setState({
+                    products: jsonResponse.data.currentProducts,
+                    currentPage: jsonResponse.data.currentPage
                 })
             )
     }
@@ -50,6 +61,7 @@ class App extends Component {
         this.callGetBagRequest();
         this.callGetFavoritesRequest();
         this.callGetBrandsRequest();
+        this.callGetProductsRequest();
     }
 
     render() {
@@ -161,6 +173,122 @@ class App extends Component {
                                     </div>
                                 </article>
                             </li>
+                            <li className="product-list__item">
+                                <article className="product" itemScope itemType="http://schema.org/Product">
+                                    <figure className="product__image-wrapper">
+                                        <img className="product__image" src="images/img01.png" alt="Product"
+                                             itemProp="image"/>
+                                        <button
+                                            className="product__wishlist-button button button--round button--wishlist">
+                                            <svg className="icon" width="20px" height="20px" viewBox="0 6 20 20"
+                                                 version="1.1" xmlns="http://www.w3.org/2000/svg"
+                                                 xmlnsXlink="http://www.w3.org/1999/xlink">
+                                                <title>Wishlist Icon</title>
+                                                <polygon id="Wishlist-Icon" stroke="none" fillRule="evenodd"
+                                                         points="12.3598869 13.2675869 20 13.2675869 13.8200565 17.7545318 16.1782804 25.0221187 9.99833694 20.5318477 3.81839348 25.0221187 6.17994346 17.7545318 0 13.2675869 7.63678696 13.2675869 9.99833694 6"/>
+                                            </svg>
+                                        </button>
+                                    </figure>
+                                    <div className="product__details">
+                                        <h1 className="product__title" itemProp="brand">Brand name</h1>
+                                        <p className="product__subtitle" itemProp="description">Product title</p>
+                                        <div className="product__price" itemScope itemType="http://schema.org/Offer">
+                                            <span className="product__price" itemProp="price">£210</span>
+                                        </div>
+                                        <button
+                                            className="product__add-to-cart button button--primary button--in-cart">In
+                                            Cart
+                                        </button>
+                                    </div>
+                                </article>
+                            </li>
+                            <li className="product-list__item">
+                                <article className="product" itemScope itemType="http://schema.org/Product">
+                                    <figure className="product__image-wrapper">
+                                        <img className="product__image" src="images/img01.png" alt="Product"
+                                             itemProp="image"/>
+                                        <button
+                                            className="product__wishlist-button button button--round button--wishlist">
+                                            <svg className="icon" width="20px" height="20px" viewBox="0 6 20 20"
+                                                 version="1.1" xmlns="http://www.w3.org/2000/svg"
+                                                 xmlnsXlink="http://www.w3.org/1999/xlink">
+                                                <title>Wishlist Icon</title>
+                                                <polygon id="Wishlist-Icon" stroke="none" fillRule="evenodd"
+                                                         points="12.3598869 13.2675869 20 13.2675869 13.8200565 17.7545318 16.1782804 25.0221187 9.99833694 20.5318477 3.81839348 25.0221187 6.17994346 17.7545318 0 13.2675869 7.63678696 13.2675869 9.99833694 6"/>
+                                            </svg>
+                                        </button>
+                                    </figure>
+                                    <div className="product__details">
+                                        <h1 className="product__title" itemProp="brand">Brand name</h1>
+                                        <p className="product__subtitle" itemProp="description">Product title</p>
+                                        <div className="product__price" itemScope itemType="http://schema.org/Offer">
+                                            <span className="product__price" itemProp="price">£210</span>
+                                        </div>
+                                        <button
+                                            className="product__add-to-cart button button--primary button--in-cart">In
+                                            Cart
+                                        </button>
+                                    </div>
+                                </article>
+                            </li>
+                            <li className="product-list__item">
+                                <article className="product" itemScope itemType="http://schema.org/Product">
+                                    <figure className="product__image-wrapper">
+                                        <img className="product__image" src="images/img01.png" alt="Product"
+                                             itemProp="image"/>
+                                        <button
+                                            className="product__wishlist-button button button--round button--wishlist">
+                                            <svg className="icon" width="20px" height="20px" viewBox="0 6 20 20"
+                                                 version="1.1" xmlns="http://www.w3.org/2000/svg"
+                                                 xmlnsXlink="http://www.w3.org/1999/xlink">
+                                                <title>Wishlist Icon</title>
+                                                <polygon id="Wishlist-Icon" stroke="none" fillRule="evenodd"
+                                                         points="12.3598869 13.2675869 20 13.2675869 13.8200565 17.7545318 16.1782804 25.0221187 9.99833694 20.5318477 3.81839348 25.0221187 6.17994346 17.7545318 0 13.2675869 7.63678696 13.2675869 9.99833694 6"/>
+                                            </svg>
+                                        </button>
+                                    </figure>
+                                    <div className="product__details">
+                                        <h1 className="product__title" itemProp="brand">Brand name</h1>
+                                        <p className="product__subtitle" itemProp="description">Product title</p>
+                                        <div className="product__price" itemScope itemType="http://schema.org/Offer">
+                                            <span className="product__price" itemProp="price">£210</span>
+                                        </div>
+                                        <button
+                                            className="product__add-to-cart button button--primary button--in-cart">In
+                                            Cart
+                                        </button>
+                                    </div>
+                                </article>
+                            </li>
+                            <li className="product-list__item">
+                                <article className="product" itemScope itemType="http://schema.org/Product">
+                                    <figure className="product__image-wrapper">
+                                        <img className="product__image" src="images/img01.png" alt="Product"
+                                             itemProp="image"/>
+                                        <button
+                                            className="product__wishlist-button button button--round button--wishlist">
+                                            <svg className="icon" width="20px" height="20px" viewBox="0 6 20 20"
+                                                 version="1.1" xmlns="http://www.w3.org/2000/svg"
+                                                 xmlnsXlink="http://www.w3.org/1999/xlink">
+                                                <title>Wishlist Icon</title>
+                                                <polygon id="Wishlist-Icon" stroke="none" fillRule="evenodd"
+                                                         points="12.3598869 13.2675869 20 13.2675869 13.8200565 17.7545318 16.1782804 25.0221187 9.99833694 20.5318477 3.81839348 25.0221187 6.17994346 17.7545318 0 13.2675869 7.63678696 13.2675869 9.99833694 6"/>
+                                            </svg>
+                                        </button>
+                                    </figure>
+                                    <div className="product__details">
+                                        <h1 className="product__title" itemProp="brand">Brand name</h1>
+                                        <p className="product__subtitle" itemProp="description">Product title</p>
+                                        <div className="product__price" itemScope itemType="http://schema.org/Offer">
+                                            <span className="product__price" itemProp="price">£210</span>
+                                        </div>
+                                        <button
+                                            className="product__add-to-cart button button--primary button--in-cart">In
+                                            Cart
+                                        </button>
+                                    </div>
+                                </article>
+                            </li>
                         </ul>
                         <nav className="pagination">
                             <ul className="pagination__list">
@@ -219,6 +347,9 @@ class App extends Component {
                 <footer className="container">
                     <p className="footer__sidenote">Footer</p>
                 </footer>
+                <p> Debug products: {this.state.products.length}</p>
+                <p> Debug currentPage: {this.state.currentPage}</p>
+                <p> Debug brands: {this.state.brands.length}</p>
             </div>
         );
     }
